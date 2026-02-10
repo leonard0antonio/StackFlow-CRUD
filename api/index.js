9b2cd5d3665ac060061c9295390383759d87dd93
DevLeoCraft <leonardoantoniosilva7700@gmail.com>
2026-02-10 18:15:26 -0300

    first method

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
+
+app.use(express.json())
 app.use(cors())
+
+app.use("/", userRoutes)
 
 app.listen(8000)
